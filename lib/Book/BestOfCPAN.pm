1043fa9df2a7d7e50595af9cf88e01563c4b158e
Sebastien Aperghis-Tramoni <sebastien@aperghis.net>
2010-05-01 22:01:37 +0200

    Import sources.

---
+++ lib/Book/BestOfCPAN.pm
@@ -0,0 +1,101 @@
+package Book::BestOfCPAN;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+__END__
+
+=head1 NAME
+
+Book::BestOfCPAN - Because the CPAN is huge
+
+
+=head1 VERSION
+
+Version 0.01
+
+
+=head1 SYNOPSIS
+
+    perldoc Book::BestOfCPAN
+
+
+=head1 DESCRIPTION
+
+=over
+
+=over
+
+=item I<"CPAN is the language, Perl is just its syntax"> -- Audrey Tang
+
+=back
+
+=back
+
+This book is written with the hope to help people who wonder which
+of the numerous modules available on the CPAN will answer the problems
+they have (and maybe event the problems they will have).
+
+
+=head1 CHAPTERS
+
+...
+
+
+=head1 AUTHOR
+
+SE<eacute>bastien Aperghis-Tramoni C<< <sebastien at aperghis.net> >>
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-book-bestofcpan at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/Public/Dist/Display.html?Dist=Book-BestOfCPAN>.
+I will be notified, and then you'll automatically be notified of
+progress on your bug as I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Book::BestOfCPAN
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/Public/Dist/Display.html?Dist=Book-BestOfCPAN>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Book-BestOfCPAN>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Book-BestOfCPAN>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Book-BestOfCPAN>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+Tim Bunce, for suggesting me to write this.
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2010 SE<eacute>bastien Aperghis-Tramoni, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
---
+++ t/00-load.t
@@ -0,0 +1,6 @@
+#!perl -T
+use strict;
+use warnings;
+use Test::More tests => 1;
+
+use_ok( "Book::BestOfCPAN" );
